2c8fbb88070dfece41e96354654fe9a996ad3b8e
Ivan De Marino <ivan.de.marino@gmail.com>
2013-07-03 18:45:07 +0100

    Defining more "stub methods" for the Page class.

--- src/java/com/amazon/aiv/sulfur/Page.java
+++ src/java/com/amazon/aiv/sulfur/Page.java
@@ -1,5 +1,7 @@
 package com.amazon.aiv.sulfur;
 
+import org.openqa.selenium.WebDriver;
+
 import java.util.List;
 
 /**
@@ -9,11 +11,25 @@ import java.util.List;
  */
 public class Page {
 
+    private WebDriver mDriver;
+
+    public Page(WebDriver driver) {
+        mDriver = driver;
+    }
+
+    public load() {
+        // TODO
+    }
+
     public String getTitle() {
         return null;
     }
 
     public String getUrl() {
+        return null;
+    }
+
+    public String getSource() {
         return null;
     }
 
@@ -23,5 +39,17 @@ public class Page {
 
     public List<PageComponent> getComponents() {
         return null;
+    }
+
+    /**
+     * Returns the Driver used by this Page.
+     * The assumption is that you know what a WebDriver is an the effect that you will have manipulating
+     * it directly.
+     *
+     * It's advisable you use PageComponent(s) instead.
+     * @return The WebDriver in use to this Page Object
+     */
+    public WebDriver getDriver() {
+        return mDriver;
     }
 }
