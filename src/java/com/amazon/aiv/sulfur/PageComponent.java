1fd9cc7568f4aa6d800a1e3e72766055993c0c36
Ivan De Marino <ivan.de.marino@gmail.com>
2013-07-03 18:45:27 +0100

    Adding more "stub methods" to the PageComponent class.

--- src/java/com/amazon/aiv/sulfur/PageComponent.java
+++ src/java/com/amazon/aiv/sulfur/PageComponent.java
@@ -1,11 +1,19 @@
 package com.amazon.aiv.sulfur;
 
+import org.openqa.selenium.WebDriver;
+
 /**
  * @author Ivan De Marino <demarino@amazon.com>
  *
  * TODO
  */
 public abstract class PageComponent {
+
+    private final WebDriver mDriver;
+
+    public PageComponent(WebDriver driver) {
+        mDriver = driver;
+    }
 
     abstract public boolean isLoaded();
 
