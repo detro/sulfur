979b21b6873dd3b87eb1db808c891a18a6148ee4
Ivan De Marino <ivan.de.marino@gmail.com>
2013-07-03 18:49:28 +0100

    Stubbing out a "WebDriverFactory".
    This will surely become a very rich class over time...

---
+++ src/java/com/amazon/aiv/sulfur/factories/WebDriverFactory.java
@@ -0,0 +1,63 @@
+package com.amazon.aiv.sulfur.factories;
+
+import com.amazon.aiv.sulfur.factories.exceptions.DriverNameInvalid;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.phantomjs.PhantomJSDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+/**
+ * @author Ivan De Marino - detronizator@gmail.com
+ *
+ * TODO
+ */
+public class WebDriverFactory {
+
+    private WebDriverFactory() {
+        // This class can't be instantiated
+    }
+
+    public static WebDriver createDriver(String driverName) {
+        switch(driverName) {
+            case Consts.DRIVERNAME_FIREFOX:
+                return createFirefoxDriver();
+            case Consts.DRIVERNAME_CHROME:
+                return createChromeDriver();
+            case Consts.DRIVERNAME_IE:
+                return createIEDriver();
+            case Consts.DRIVERNAME_PHANTOMJS:
+                return createPhantomJSDriver();
+            case Consts.DRIVERNAME_OPERA:
+                return createOperaDriver();
+            default:
+                throw new DriverNameInvalid(driverName);
+        }
+    }
+
+    public static WebDriver createFirefoxDriver() {
+        // TODO - too basic at the moment
+        return new FirefoxDriver();
+    }
+
+    public static WebDriver createChromeDriver() {
+        // TODO - too basic at the moment
+        return new ChromeDriver();
+    }
+
+    public static WebDriver createIEDriver() {
+        // TODO - too basic at the moment
+        return new InternetExplorerDriver();
+    }
+
+    public static WebDriver createOperaDriver() {
+        // TODO
+        return null;
+    }
+
+    public static WebDriver createPhantomJSDriver() {
+        // TODO - too basic at the moment
+        return new PhantomJSDriver(DesiredCapabilities.phantomjs());
+    }
+}
