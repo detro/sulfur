d9ffdd2262f6785aa697a051fe4b0b63991d7659
Ivan De Marino <ivan.de.marino@gmail.com>
2013-07-03 18:44:44 +0100

    Adding "Driver Names" to the Consts.

--- src/java/com/amazon/aiv/sulfur/factories/Consts.java
+++ src/java/com/amazon/aiv/sulfur/factories/Consts.java
@@ -8,9 +8,15 @@ package com.amazon.aiv.sulfur.factories;
 public interface Consts {
     /** MANDATORY System Property to instruct Sulfur where to look for Page Configs files */
     public static final String SYSPROP_PAGE_CONFIGS_DIR_PATH    = "sulfur.page.configs";
+    /** MANDATORY System Property to instruct Sulfur where to look for the Config file */
+    public static final String SYSPROP_CONFIG_FILE_PATH = "sulfur.config";
+
     /** MANDATORY Extension that Page Config files have to use */
     public static final String EXTENSION_PAGE_CONFIG_FILE = ".sulfur.page.config.json";
 
-    /** MANDATORY System Property to instruct Sulfur where to look for the Config file */
-    public static final String SYSPROP_CONFIG_FILE_PATH = "sulfur.config";
+    public static final String DRIVERNAME_FIREFOX = "firefox";
+    public static final String DRIVERNAME_CHROME = "chrome";
+    public static final String DRIVERNAME_IE = "ie";
+    public static final String DRIVERNAME_PHANTOMJS = "phantomjs";
+    public static final String DRIVERNAME_OPERA = "opera";
 }
