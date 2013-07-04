b9269bea9aad7aa2e36eadfc70a7f51286eb8c3c
Ivan De Marino <ivan.de.marino@gmail.com>
2013-07-04 16:38:19 +0100

    Added logic to validate Path and Query Parameters to the Page.
    Also, the Page object provides utility method to compose the final URL Path and Query
    based on the validation and the parameters Map provided.

--- src/java/com/amazon/aiv/sulfur/factories/PageConfig.java
+++ src/java/com/amazon/aiv/sulfur/factories/PageConfig.java
@@ -1,10 +1,10 @@
 package com.amazon.aiv.sulfur.factories;
 
+import com.amazon.aiv.sulfur.factories.exceptions.MissingPathParamException;
+import com.amazon.aiv.sulfur.factories.exceptions.MissingQueryParamException;
 import org.apache.log4j.Logger;
 
-import java.util.Arrays;
-import java.util.LinkedHashSet;
-import java.util.Set;
+import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -36,16 +36,16 @@ public class PageConfig {
     private transient LinkedHashSet<String>     queryMandatoryParams = null;
 
     // Patterns to extract Parameters from Path
-    private static final String PATTERN_PATH_PARAM = "\\{!?(\\w+)\\}";
-    private static final String PATTERN_PATH_MANDATORY_PARAM = "\\{!(\\w+)\\}";
+    private static final String PATTERN_FORMAT_PATH_PARAM = "\\{!?(%s)\\}";
+    private static final String PATTERN_FORMAT_PATH_MANDATORY_PARAM = "\\{!(%s)\\}";
 
     /**
      * Extract Parameters from the Path, based on a given Pattern String.
      * The Parameters are portion of a URL Path, surrounded by curly brackets.
      * Example: "/example/path/{param1}/and/{param2}".
      *
-     * @see PageConfig#PATTERN_PATH_PARAM
-     * @see PageConfig#PATTERN_PATH_MANDATORY_PARAM
+     * @see PageConfig#PATTERN_FORMAT_PATH_PARAM
+     * @see PageConfig#PATTERN_FORMAT_PATH_MANDATORY_PARAM
      *
      * @param patternStr String containing the Pattern that will be used to instantiate an actual Pattern object
      * @return A Set containing the Parameter found.
@@ -54,7 +54,7 @@ public class PageConfig {
         LinkedHashSet<String> paramsSet = new LinkedHashSet<String>();
 
         // Extract all the Path Mandatory Parameters and store them locally
-        Matcher pathMandatoryParamsMatcher = Pattern.compile(patternStr).matcher(getPath());
+        Matcher pathMandatoryParamsMatcher = Pattern.compile(String.format(patternStr, "\\w+")).matcher(getPath());
         while (pathMandatoryParamsMatcher.find()) {
             paramsSet.add(pathMandatoryParamsMatcher.group(1));
         }
@@ -94,20 +94,6 @@ public class PageConfig {
         return path;
     }
 
-    public Set<String> getQueryParams() {
-        if (null == queryMandatoryParams) {
-            initQueryMandatoryParams();
-        }
-        return queryParams;
-    }
-
-    public Set<String> getQueryMandatoryParamsSet() {
-        if (null == queryMandatoryParams) {
-            initQueryMandatoryParams();
-        }
-        return queryMandatoryParams;
-    }
-
     public String getFilename() {
         return filename;
     }
@@ -116,16 +102,30 @@ public class PageConfig {
         this.filename = filename;
     }
 
-    public Set<String> getPathParamsSet() {
+    public Set<String> getQueryParams() {
+        if (null == queryMandatoryParams) {
+            initQueryMandatoryParams();
+        }
+        return queryParams;
+    }
+
+    public Set<String> getQueryMandatoryParams() {
+        if (null == queryMandatoryParams) {
+            initQueryMandatoryParams();
+        }
+        return queryMandatoryParams;
+    }
+
+    public Set<String> getPathParams() {
         if (null == pathParams) {
-            pathParams = extractPathParams(PATTERN_PATH_PARAM);
+            pathParams = extractPathParams(PATTERN_FORMAT_PATH_PARAM);
         }
         return pathParams;
     }
 
-    public Set<String> getPathMandatoryParamsSet() {
+    public Set<String> getPathMandatoryParams() {
         if (null == pathMandatoryParams) {
-            pathMandatoryParams = extractPathParams(PATTERN_PATH_MANDATORY_PARAM);
+            pathMandatoryParams = extractPathParams(PATTERN_FORMAT_PATH_MANDATORY_PARAM);
         }
         return pathMandatoryParams;
     }
@@ -135,9 +135,64 @@ public class PageConfig {
         logger.debug("  name: " + getName());
         logger.debug("  components: " + Arrays.toString(getComponents()));
         logger.debug("  path: " + getPath());
-        logger.debug("  pathParams: " + getPathParamsSet());
-        logger.debug("  pathMandatoryParams: " + getPathMandatoryParamsSet());
+        logger.debug("  pathParams: " + getPathParams());
+        logger.debug("  pathMandatoryParams: " + getPathMandatoryParams());
         logger.debug("  queryParams: " + getQueryParams());
-        logger.debug("  queryMandatoryParams: " + getQueryMandatoryParamsSet());
+        logger.debug("  queryMandatoryParams: " + getQueryMandatoryParams());
+    }
+
+    private void checkPathParams(Map<String, String> pathParams) {
+        // Check all the Mandatory Path Parameters are provided
+        for (String pathMandatoryParam : getPathMandatoryParams()) {
+            if (!pathParams.containsKey(pathMandatoryParam)) {
+                throw new MissingPathParamException(pathMandatoryParam);
+            }
+        }
+    }
+
+    private void checkQueryParams(Map<String, String> queryParams) {
+        // Check all the Mandatory Query Parameters are provided
+        for (String queryMandatoryParam : getQueryMandatoryParams()) {
+            if (!queryParams.containsKey(queryMandatoryParam)) {
+                throw new MissingQueryParamException(queryMandatoryParam);
+            }
+        }
+    }
+
+    public String composeUrlPath(Map<String, String> pathParams) {
+        // Check all MANDATORY the Path Params are provided
+        checkPathParams(pathParams);
+
+        // If we reach here, everything is fine and we can compose the URL Path
+        String resultPath = getPath();
+
+        // Replace parameters in the PageConfig Path with the given value
+        for (Map.Entry<String, String> pathParam : pathParams.entrySet()) {
+            resultPath.replaceFirst(String.format(PATTERN_FORMAT_PATH_PARAM, pathParam.getKey()), pathParam.getValue());
+        }
+        // Replace all the remaining parameter (not set with the previous loop) with an empty string
+        resultPath.replaceAll(String.format(PATTERN_FORMAT_PATH_PARAM, ""), "");
+
+        return resultPath;
+    }
+
+    public String composeUrlQuery(Map<String, String> queryParams) {
+        // Check all MANDATORY the Query Params are provided
+        checkQueryParams(queryParams);
+
+        StringBuilder resultQuery = new StringBuilder();
+
+        Iterator<Map.Entry<String, String>> i = queryParams.entrySet().iterator();
+        while (i.hasNext()) {
+            Map.Entry<String, String> queryParam = i.next();
+
+            resultQuery.append(queryParam.getKey());
+            resultQuery.append("=");
+            resultQuery.append(queryParam.getValue());
+
+            resultQuery.append(i.hasNext() ? "&" : "");
+        }
+
+        return resultQuery.toString();
     }
 }
