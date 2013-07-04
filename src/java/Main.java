4b5ac6ba99c88b6472664ef3e45a575e88c52dbb
Ivan De Marino <ivan.de.marino@gmail.com>
2013-07-04 17:38:18 +0100

    Removing "Main" class.
    Was useful initially to validate stuff worked, but now let's write some proper tests.

--- src/java/Main.java
+++
@@ -1,8 +0,0 @@
-import com.amazon.aiv.sulfur.factories.PageConfigFactory;
-import com.amazon.aiv.sulfur.factories.PageFactory;
-
-public class Main {
-    public static void main(String[] argv) throws Exception {
-        PageFactory.getInstance();
-    }
-}
