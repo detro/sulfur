d3e1c0b6a9df5f2a91187923fb200c018fe2769a
Ivan De Marino <ivan.de.marino@gmail.com>
2013-07-04 18:35:23 +0100

    Refactoring "InvalidParametersForPageCreationTest" to grab the PageFactory instance directly from the Factory and not from BaseTest.

--- tst/java/com/amazon/aiv/sulfur/test/InvalidParametersForPageCreationTest.java
+++ tst/java/com/amazon/aiv/sulfur/test/InvalidParametersForPageCreationTest.java
@@ -2,15 +2,15 @@ package com.amazon.aiv.sulfur.test;
 
 import com.amazon.aiv.sulfur.BaseTest;
 import com.amazon.aiv.sulfur.Page;
+import com.amazon.aiv.sulfur.factories.Consts;
+import com.amazon.aiv.sulfur.factories.PageFactory;
 import com.amazon.aiv.sulfur.factories.exceptions.MissingPathParamException;
 import com.amazon.aiv.sulfur.factories.exceptions.MissingQueryParamException;
 import com.amazon.aiv.sulfur.factories.exceptions.UnavailableDriverException;
 import com.amazon.aiv.sulfur.factories.exceptions.UnavailablePageException;
-import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import java.util.HashMap;
-import java.util.Iterator;
 
 /**
  * Tests to check that Invalid Parameters, provided at Page-creation time, cause the expected failure.
@@ -18,9 +18,15 @@ import java.util.Iterator;
  * @author Ivan De Marino - demarino@amazon.com
  */
 public class InvalidParametersForPageCreationTest extends BaseTest {
+
+    public InvalidParametersForPageCreationTest() {
+        System.setProperty(Consts.SYSPROP_PAGE_CONFIGS_DIR_PATH, "tst/ex01.page.configs");
+        System.setProperty(Consts.SYSPROP_CONFIG_FILE_PATH, "tst/ex01.sulfur.config.json");
+    }
+
     @Test(dataProvider = "driverProvider", expectedExceptions = MissingQueryParamException.class)
     public void shouldFailIfMandatoryQueryParameterIsMissing(String driverName) {
-        Page playerSoloMode = getPageFactory().createPage(
+        Page playerSoloMode = PageFactory.getInstance().createPage(
                 driverName,
                 "playersolomode",
                 new HashMap<String, String>(),
@@ -30,7 +36,7 @@ public class InvalidParametersForPageCreationTest extends BaseTest {
 
     @Test(dataProvider = "driverProvider", expectedExceptions = MissingPathParamException.class)
     public void shouldFailIfMandatoryPathParameterIsMissing(String driverName) {
-        Page playerSoloMode = getPageFactory().createPage(
+        Page playerSoloMode = PageFactory.getInstance().createPage(
                 driverName,
                 "detailspage",
                 new HashMap<String, String>(),
@@ -40,7 +46,7 @@ public class InvalidParametersForPageCreationTest extends BaseTest {
 
     @Test(dataProvider = "pageProvider", expectedExceptions = UnavailableDriverException.class)
     public void shouldFailIfDriverNameIsInvalid(String pageName) {
-        Page playerSoloMode = getPageFactory().createPage(
+        Page playerSoloMode = PageFactory.getInstance().createPage(
                 "madeUpDriver",
                 pageName,
                 new HashMap<String, String>(),
@@ -50,7 +56,7 @@ public class InvalidParametersForPageCreationTest extends BaseTest {
 
     @Test(dataProvider = "driverProvider", expectedExceptions = UnavailablePageException.class)
     public void shouldFailIfPageNameIsInvalid(String driverName) {
-        Page playerSoloMode = getPageFactory().createPage(
+        Page playerSoloMode = PageFactory.getInstance().createPage(
                 driverName,
                 "nonExistentPage",
                 new HashMap<String, String>(),
