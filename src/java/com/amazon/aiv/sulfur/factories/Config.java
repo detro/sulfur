6445677a5a708fefddbf44425ded101c7840666b
Ivan De Marino <ivan.de.marino@gmail.com>
2013-07-04 14:47:08 +0100

    Adding "selenium hub" as a possible Sulfur config parameter.
    Also, starting to use "LinkedHashSet" in place of plain arrays, to make "containment checks" faster.

--- src/java/com/amazon/aiv/sulfur/factories/Config.java
+++ src/java/com/amazon/aiv/sulfur/factories/Config.java
@@ -2,7 +2,9 @@ package com.amazon.aiv.sulfur.factories;
 
 import org.apache.log4j.Logger;
 
-import java.util.Arrays;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.*;
 
 /**
  * This is a POJO, intended to be used with Google GSON.
@@ -18,9 +20,11 @@ import java.util.Arrays;
  * TODO
  */
 public class Config {
-    private String      host = null;
-    private int         port = 80;
-    private String[]    drivers = null;
+    // JSON
+    private String                  host = null;
+    private int                     port = 80;
+    private LinkedHashSet<String>   drivers = null; //< makes "drivers" lookup fast
+    private String                  seleniumhub = null;
 
     public String getHost() {
         return host;
@@ -30,13 +34,30 @@ public class Config {
         return port;
     }
 
-    public String[] getDrivers() {
+    public Set<String> getDrivers() {
         return drivers;
     }
 
+    /**
+     * Returns the URL to the Selenium HUB to use for our tests.
+     *
+     * @return URL to Selenium HUB to use, or "null" in case parameter not configured or malformed
+     */
+    public URL getSeleniumHub() {
+        if (null != seleniumhub) {
+            try {
+                return new URL(seleniumhub);
+            } catch(MalformedURLException mue) {
+                // swallow the exception: we fall back to "null" in case the URL is malformed
+            }
+        }
+        return null;
+    }
+
     public void logDebug(Logger logger) {
-        logger.debug("host: " + getHost());
-        logger.debug("port: " + getPort());
-        logger.debug("drivers: " + Arrays.toString(getDrivers()));
+        logger.debug("  host: " + getHost());
+        logger.debug("  port: " + getPort());
+        logger.debug("  driver: " + getDrivers());
+        logger.debug("  seleniumhub: " + getSeleniumHub());
     }
 }
