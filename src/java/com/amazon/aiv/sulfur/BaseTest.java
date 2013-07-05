6f20ac720f1953bb9b630be2091bf4cb07a45406
Ivan De Marino <ivan.de.marino@gmail.com>
2013-07-05 10:45:39 +0100

    Added utility in BaseTest to automatically take care of closing pages in case a test fails and the Page's Driver is "hanging".

--- src/java/com/amazon/aiv/sulfur/BaseTest.java
+++ src/java/com/amazon/aiv/sulfur/BaseTest.java
@@ -3,6 +3,8 @@ package com.amazon.aiv.sulfur;
 import com.amazon.aiv.sulfur.factories.PageFactory;
 import com.amazon.aiv.sulfur.utils.DataProviderUtils;
 import com.google.common.collect.ImmutableList;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.DataProvider;
 
 import java.util.*;
@@ -11,6 +13,45 @@ import java.util.*;
  * @author Ivan De Marino <demarino@amazon.com>
  */
 public class BaseTest {
+
+    private List<Page> mPagesToCloseAfterTest = new ArrayList<Page>();
+
+    /**
+     * Utility method to ensure, even on test failure, the Page object is correctly closed (i.e. "driver.quit()" is invoked).
+     *
+     * @see com.amazon.aiv.sulfur.BaseTest#closePagesAfterMethod()
+     * @param page Page to close after the test.
+     */
+    protected Page doCloseAfterMethod(Page page) {
+        mPagesToCloseAfterTest.add(page);
+        return page;
+    }
+
+    @AfterMethod
+    protected void closePagesAfterMethod() {
+        for (Page p : mPagesToCloseAfterTest) {
+            p.close();
+        }
+    }
+
+    /**
+     * Utility method that creates a Page that is already listed as a "closing after method".
+     * Signature reflects the @see PageFactory#createPage signature.
+     *
+     * @param driverName
+     * @param pageName
+     * @param pathParams
+     * @param queryParams
+     * @return Page that is already added to the list of Page that will "close after method".
+     */
+    protected Page createSelfClosingPage(String driverName,
+                                         String pageName,
+                                         Map<String, String> pathParams,
+                                         Map<String, String> queryParams) {
+        return doCloseAfterMethod(PageFactory.getInstance().createPage(
+                driverName, pageName,
+                pathParams, queryParams));
+    }
 
     /**
      * @DataProvider of all the Pages found by the PageConfigFactory.
